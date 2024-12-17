95fd02bf1874f55dbfeceafa75984e0097af3559
AngeleMarie <angelarma2006@gmail.com>
2024-12-17 08:42:52 +0200

    made changes

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(session({
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:3000"],  
+  origin: ["http://localhost:8081"],  
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
