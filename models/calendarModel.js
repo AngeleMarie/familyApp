6ef7d61793e99c5269a1629ebd8d51d950de2ed4
AngeleMarie <angelarma2006@gmail.com>
2024-12-17 10:34:31 +0200

    made changes

--- models/calendarModel.js
+++ models/calendarModel.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
 const calendarEventSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  date: { type: Date, required: true },
-  time: { type: String },
-  createdBy: { type: String, required: true },
+  eventName: { type: String, required: true },
+  eventDate: { type: Date, required: true },
+ eventTime: { type: String }
+  
 });
 
 export default mongoose.model('CalendarEvent', calendarEventSchema);
