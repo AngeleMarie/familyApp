0920ac0cd2b80f373b2d8d6092c23ae92722cd5e
AngeleMarie <angelarma2006@gmail.com>
2024-12-17 15:28:30 +0200

    made changes

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 
 const shoppingItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  quantity: { type: Number, required: true, default: 1 },
-  purchased: { type: Boolean, default: false },
   category: { type: String, required: true },
   
 });
